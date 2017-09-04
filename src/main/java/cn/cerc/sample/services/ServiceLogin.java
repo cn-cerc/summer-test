13aa2bbe9dc80779cf223ec4877a6c7e4036ed7c
张弓 <sz9214e@qq.com>
2017-09-04 15:53:51 +0800

    配合summer-android的RemoteService范例调用

--- src/main/java/cn/cerc/sample/services/ServiceLogin.java
+++ src/main/java/cn/cerc/sample/services/ServiceLogin.java
@@ -12,13 +12,16 @@ public class ServiceLogin extends AbstractService {
     @Override
     public IStatus execute(DataSet dataIn, DataSet dataOut) throws ServiceException {
         Record headIn = dataIn.getHead();
-        String userCode = headIn.getString("userCode");
+        String userCode = headIn.getString("usercode");
         String password = headIn.getString("password");
         if (!"admin".equals(userCode))
             return fail("用户帐号不存在！");
-        if (!"admin".equals(password))
-            return fail("用户帐号不存在！");
+        if (!"123456".equals(password))
+            return fail("用户密码不正确！");
         dataOut.getHead().setField("token", "000000");
+        dataOut.append();
+        dataOut.setField("num", 123);
+        dataOut.setField("remark", "OK");
         return this.success();
     }
 
