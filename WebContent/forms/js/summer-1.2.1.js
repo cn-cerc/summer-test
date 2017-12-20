7ee4093420487994ea501f2de5ea9c2a1cf8a335
张弓 <sz9214e@qq.com>
2017-12-20 13:16:52 +0800

    改进getBrowser函数及其实现

--- WebContent/forms/js/summer-1.2.1.js
+++ WebContent/forms/js/summer-1.2.1.js
@@ -2,23 +2,48 @@
  * 配合summer-android, summer-iphone项目使用
  */
 
-function AndroidProxy() { // 安卓代理，对应android源码中的JavaScriptProxy
+function ClientProxy() { // 安卓代理，对应android源码中的JavaScriptProxy
 	this.active = false; // 标识为true时，表示当前为summer-android专用浏览器
 	this.machine = null; // 当前的JavaScriptProxy对象
 	this.req = {}; // 请求参数
 	this.resp = {}; // 返回参数
 	this.message = null; // 返回的消息
+	this.device = "browser";
 
+	//android 浏览器
 	if (window.JSobj) { // 兼容老的JSobj
 		this.machine = window.JSobj;
+		this.device="android";
 		this.active = true;
-	}
-	if (window.jsAndroid) { // 原JSobj改名为jsAndroid
+	}else if (window.jsAndroid) { // 原JSobj改名为jsAndroid
 		this.machine = window.jsAndroid;
+        this.device="android";
 		this.active = true;
 	}
-
-	this.support = function(classCode) { // 判断是否支持指定的功能函数
+	// iphone 浏览器
+	if(!this.active){
+	    var ua = navigator.userAgent.toLowerCase();
+	    if (/iphone|ipad|ipod/.test(ua)){
+	        this.device="iphone";
+	        this.active = true;
+	    }
+	}
+	//c#浏览器
+	if(!this.active){
+    	if(window.external){ 
+            this.machine = window.external;
+            try{
+                window.external.send("GetVersionName", "{}");
+                this.device="window";
+                this.active = true;
+            } catch (e) {
+                this.active = false;
+            }
+    	}
+	}
+	
+	// 判断是否支持指定的功能函数
+	this.support = function(classCode) { 
 		if (!this.active) {
 			this.message = "您的版本太旧，不支持指令：" + classCode;
 			return false;
@@ -35,6 +60,7 @@ function AndroidProxy() { // 安卓代理，对应android源码中的JavaScriptP
 		}
 	}
 
+	//列出所有可用的服务
 	this.list = function(){
 		if (!this.active)
 			return null;
@@ -45,16 +71,28 @@ function AndroidProxy() { // 安卓代理，对应android源码中的JavaScriptP
 			return null;
 	}
 	
-	this.send = function(classCode) {// 调用 android 中的功能函数，并立即返回
+	//请求具体的服务，并立即返回
+	this.send = function(classCode) {
 		if (!this.active)
 			return null;
 		try {
-			var dataOut = this.machine.send(classCode, JSON.stringify(this.req));
-			this.resp = JSON.parse(dataOut);
+			var dataOut = {};
+			if(this.device == "iphone"){
+		        this.req.classCode = classCode;
+		        window.webkit.messageHandlers.nativeMethod.postMessage(this.req);
+	            this.resp = {
+	                    "result" : true,
+	                    "message" : "finish"
+	                };
+			}else{
+			    dataOut = this.machine.send(classCode, JSON.stringify(this.req));
+			    this.resp = JSON.parse(dataOut);
+			}
+			
 			return this.resp.result;
 		} catch (e) {
 			this.resp = {
-			    "result" : true,
+			    "result" : false,
 			    "message" : e.message
 			};
 			return this.resp.result;
@@ -79,32 +117,11 @@ function AndroidProxy() { // 安卓代理，对应android源码中的JavaScriptP
 	}
 }
 
-// IPhone代理，对应IPhone源码中的JavaScriptProxy
-function IPhoneProxy() {
-	this.active = false; // 标识为true时，表示当前为summer-iphone专用浏览器
-	this.req = {}; // 请求参数
-	this.resp = {}; // 返回参数
-	this.message = null; // 返回的消息
-
-	var ua = navigator.userAgent.toLowerCase();
-	if (/iphone|ipad|ipod/.test(ua)){
-		this.active = true;
-	}
-	
-	this.send = function(classCode){
-		this.req.classCode = classCode;
-		window.webkit.messageHandlers.nativeMethod.postMessage(this.req);
-		return "ok";
-	}
-}
-
 // 自动判断并返回summer-android或summer-ihone专用浏览器
 function getBrowser() {
-	browser = new AndroidProxy();
+	browser = new ClientProxy();
 	if (!browser.active) {
-		browser = new IPhoneProxy();
-		if (!browser.active)
-			return null;
+	    return null;
 	}
 	return browser;
 }
