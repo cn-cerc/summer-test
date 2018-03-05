b7ea706a3ed02ccd30e0b78a2461a0fb8ec213db
LWAndy <andychr@163.com>
2018-03-05 20:55:26 +0800

    完成delete

--- src/main/java/cn/cerc/sample/common/BaseConfig.java
+++ src/main/java/cn/cerc/sample/common/BaseConfig.java
@@ -3,5 +3,5 @@ package cn.cerc.sample.common;
 public class BaseConfig {
 
     public static final String product = "product";
-    public static final String CorpNo = "913001";
+    public static final String CorpNo = "916001";
 }
