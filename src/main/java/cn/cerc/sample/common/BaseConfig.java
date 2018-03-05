72112fb3ef521873c82b2dbea408649e50d684d7
lsw <lsw159134@163.com>
2018-03-05 18:56:55 +0800

    商品的查询

---
+++ src/main/java/cn/cerc/sample/common/BaseConfig.java
@@ -0,0 +1,7 @@
+package cn.cerc.sample.common;
+
+public class BaseConfig {
+
+	public static final String Product= "PRODUCT";
+	public static final String CorpNo= "913001";
+}
---
+++ src/main/java/cn/cerc/sample/pojo/Product.java
@@ -0,0 +1,43 @@
+package cn.cerc.sample.pojo;
+
+public class Product {
+
+	private String corpNo;
+	private String code;
+	private String spec;
+	private String uint;
+	private Double stock;
+	public String getCorpNo() {
+		return corpNo;
+	}
+	public void setCorpNo(String corpNo) {
+		this.corpNo = corpNo;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getSpec() {
+		return spec;
+	}
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+	public String getUint() {
+		return uint;
+	}
+	public void setUint(String uint) {
+		this.uint = uint;
+	}
+	public Double getStock() {
+		return stock;
+	}
+	public void setStock(Double stock) {
+		this.stock = stock;
+	}
+	
+	
+	
+}
