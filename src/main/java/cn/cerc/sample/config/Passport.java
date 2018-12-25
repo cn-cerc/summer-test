c695b85b0ca92b6ffd63eaa4673694b4c13b4625
ZhangGong <sz9214e@qq.com>
2018-12-25 14:25:01 +0800

    Update Passport.java

--- src/main/java/cn/cerc/sample/config/Passport.java
+++ src/main/java/cn/cerc/sample/config/Passport.java
@@ -2,37 +2,9 @@ package cn.cerc.sample.config;
 
 import org.springframework.stereotype.Component;
 
-import cn.cerc.mis.core.AbstractHandle;
-import cn.cerc.mis.core.IPassport;
-import cn.cerc.mis.rds.PassportRecord;
+import cn.cerc.mis.core.PassportDefault;
 
 @Component
-public class Passport extends AbstractHandle implements IPassport {
-
-    @Override
-    public boolean passProc(String versions, String procCode) {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public boolean passAction(String procCode, String action) {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public PassportRecord getRecord(String procCode) {
-        // TODO Auto-generated method stub
-        PassportRecord result = new PassportRecord();
-        result.setAdmin(true);
-        return result;
-    }
-
-    @Override
-    public boolean passsMenu(String menuCode) {
-        // TODO Auto-generated method stub
-        return true;
-    }
+public class Passport extends PassportDefault {
 
 }
