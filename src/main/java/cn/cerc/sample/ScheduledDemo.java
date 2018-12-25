bb81e38a822ac52f555b50065624469c883fcaec
ZhangGong <sz9214e@qq.com>
2018-12-25 15:06:56 +0800

    Create ScheduledDemo.java

---
+++ src/main/java/cn/cerc/sample/ScheduledDemo.java
@@ -0,0 +1,17 @@
+package cn.cerc.sample;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import cn.cerc.core.TDateTime;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ScheduledDemo {
+
+    @Scheduled(fixedDelay = 1000)
+    public void run() {
+        log.info("{}", "" + TDateTime.Now());
+    }
+}
