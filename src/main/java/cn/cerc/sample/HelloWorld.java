8db7c34c103c0e00cdc55ada718d57e7e81daef9
ZhangGong <sz9214e@qq.com>
2019-01-03 14:42:06 +0800

    清除WebHandle

--- src/main/java/cn/cerc/sample/HelloWorld.java
+++ src/main/java/cn/cerc/sample/HelloWorld.java
@@ -8,16 +8,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
 
+import cn.cerc.sample.config.Handle;
+
 @RestController
 @Scope(WebApplicationContext.SCOPE_REQUEST)
 @RequestMapping("/app")
 public class HelloWorld {
     @Autowired
-    private WebHandle handle;
+    private Handle handle;
 
     @RequestMapping("/FrmTest.{func}")
     @ResponseBody
     public String hello(@PathVariable String func) {
-        return handle.getName() + ":" + func;
+        return handle.getUserName() + ":" + func;
     }
 }
